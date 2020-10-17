def42ea77bfeb78714b42f9c286afa4a728d533b
Rabbi <alrabbism@gmail.com>
2020-10-17 19:31:13 +0600

    all done

--- index.js
+++ index.js
@@ -4,177 +4,168 @@ const cors = require('cors');
 const fs = require('fs-extra');
 const fileUpload = require('express-fileupload');
 const MongoClient = require('mongodb').MongoClient;
-require('dotenv').config();
+require('dotenv').config()
 
+const app = express()
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.elm9b.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
-
-
-const app = express();
 app.use(bodyParser.json());
 app.use(cors());
-app.use(express.static('service'));
+app.use(express.static('orders'));
 app.use(fileUpload());
-
-const admin = require('firebase-admin');
-
-const serviceAccount = require("./config/creative-agency-200b7-firebase-adminsdk-j2xhn-a8dac3f4c3.json");
-const { ObjectId } = require('mongodb');
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://creative-agency-200b7.firebaseio.com"
-});
 
 const port = 5000;
 
+app.get('/', (req, res) => {
+    res.send("Mongo Data Base Working")
+})
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
 client.connect(err => {
-  const servicesCollection = client.db("creativeAgency").collection("services");
-  const feedbackCollection = client.db("creativeAgency").collection("feedback");
-  const userOrderCollection = client.db("creativeAgency").collection("userOrder");
-  const adminCollection = client.db("creativeAgency").collection("admin");
+    const serviceCollection = client.db("creativeAgency").collection("services");
+    const orderCollection = client.db("creativeAgency").collection("orders");
+    const reviewCollection = client.db("creativeAgency").collection("reviews");
+    const adminCollection = client.db("creativeAgency").collection("admins");
 
-  
-    app.get('/services', (req, res) => {
-    servicesCollection.find({})
-        .toArray((err, documents) => {
-            res.send(documents);
-        })
-    })
+    app.post('/addService', (req, res) => {
+        const file = req.files.file;
+        const name = req.body.name;
+        const description = req.body.description;
+        const newImg = file.data;
+        const encImg = newImg.toString('base64');
 
+        var image = {
+            contentType: file.mimetype,
+            size: file.size,
+            img: Buffer.from(encImg, 'base64')
+        };
 
-    app.get('/feedback', (req, res) => {
-    feedbackCollection.find({})
-        .toArray((err, documents) => {
-            res.send(documents);
-        })
-    })
-
-    app.post('/addUserOrder', (req, res) => {
-        const events = req.body;
-        userOrderCollection.insertOne(events)
+        serviceCollection.insertOne({ name, description, image })
             .then(result => {
-                res.send(result.insertedCount);
+                res.send(result.insertedCount > 0);
             })
+    });
+
+    // app.post('/addReview', (req, res) => {
+    //     const review = req.body;
+    //     reviewCollection.insertMany(review)
+    //     .then(result => {
+    //         res.send(result.insertedCount)
+    //     })
+    // })
+
+    app.get('/services', (req, res) => {
+        serviceCollection.find({})
+        .toArray((err, documents) => {
+            res.send(documents);
+        })
     })
 
-    app.get('/userOrder', (req, res) => {
-        
-        const bearer = req.headers.authorization;
-        if (bearer && bearer.startsWith('Bearer ')) {
-            const idToken = bearer.split(' ')[1];
-            admin.auth().verifyIdToken(idToken)
-                .then(function (decodedToken) {
-                    const tokenEmail = decodedToken.email;
-                    const queryEmail = req.query.email;
-                    if (tokenEmail == queryEmail) {
-                        userOrderCollection.find({ email: queryEmail })
-                            .toArray((err, documents) => {
-                                res.status(200).send(documents);
-                            })
-                    }
-                    else{
-                        res.status(401).send('Unauthorized access');
-                    }
-                    // ...
-                }).catch(function (error) {
-                    res.status(401).send('Unauthorized access');
-                });
-        }
-        else{
-            res.status(401).send('Unauthorized access');
-        }
-    })
+
 
     app.post('/addReview', (req, res) => {
-        const events = req.body;
-        feedbackCollection.insertOne(events)
-            .then(result => {
-                res.send(result.insertedCount);
-            })
+        const review = req.body;
+        reviewCollection.insertOne(review)
+        .then(result => {
+            res.send(result.insertedCount > 0)
+        })
+    });
+
+    app.get('/reviews', (req, res) => {
+        reviewCollection.find({})
+        .toArray((err, documents) => {
+            res.send(documents);
+        })
     })
 
-    app.get('/orders', (req, res) => {
-    userOrderCollection.find({})
+    app.post('/addAdmin', (req, res) => {
+        const admin = req.body;
+        adminCollection.insertOne(admin)
+        .then(result => {
+            res.send(result.insertedCount > 0)
+        })
+    });
+
+    app.get('/admins', (req, res) => {
+        adminCollection.find({})
         .toArray((err, documents) => {
             res.send(documents);
         })
     })
 
     app.post('/addOrder', (req, res) => {
+        
         const file = req.files.file;
         const name = req.body.name;
         const email = req.body.email;
-        const project = req.body.project;
-        const details = req.body.details;
         const price = req.body.price;
-        const status = req.body.status;
-            const newImg = file.data;
-            const encImg = newImg.toString('base64');
+        const details = req.body.details;
+        const design = req.body.design;
+        const newImg = file.data;
+        const encImg = newImg.toString('base64');
 
-            const image= {
-                contentType: file.mimetype,
-                size: file.size,
-                img: Buffer.from(encImg, 'base64')
-            };
-            userOrderCollection.insertOne({name, email, project, details, price, status, image})
-            .then(result => {
-                    res.send(result.insertedCount > 0);
-            })
-        
-    })
+        var image = {
+            contentType: file.mimetype,
+            size: file.size,
+            img: Buffer.from(encImg, 'base64')
+        };
 
-    app.post('/addService', (req, res) => {
-        const file = req.files.file;
-        const title = req.body.title;
-        const description = req.body.description;
-        
-            const newImg = file.data;
-            const encImg = newImg.toString('base64');
-
-            const image = {
-                contentType: file.mimetype,
-                size: file.size,
-                img: Buffer.from(encImg, 'base64')
-            };
-            servicesCollection.insertOne({title, description, image})
-            .then(result => {
-                res.send(result.insertedCount > 0);
-            })
-        
-    })
-
-    app.post('/addAdmin', (req, res) => {
-        const events = req.body;
-        adminCollection.insertOne(events)
-            .then(result => {
-                res.send(result.insertedCount);
-            })
-    })
-    app.get('/admins', (req, res) => {
-        adminCollection.find({})
-            .toArray((err, documents) => {
-                res.send(documents);
-            })
-        })
-
-    app.patch(`/updateOrders`, (req, res) => {
-        userOrderCollection.updateOne({_id: ObjectId(req.body.id)},
-        {
-            $set: {status: req.body.status}
-        })
-        .then (result => {
-            console.log(result);
+        orderCollection.insertOne({ name, email, image, price, details, design })
+        .then(result => {
+            res.send(result.insertedCount > 0);
         })
     })
+
+    app.get('/orderList', (req, res) => {
+        orderCollection.find({})
+        .toArray( (err, documents) => {
+            res.send(documents)
+        })
+    })
+
+
+    app.get('/orderByEmail', (req, res) => {
+        //   console.log(req.query.email)
+        // console.log(req.headers.authorization)
+        const bearer = req.headers.authorization;
+        
+        if(bearer && bearer.startsWith('Bearer ')){
+          const idToken = bearer.split(' ')[1];
+          console.log({idToken})
+          admin.auth().verifyIdToken(idToken)
+          .then(function(decodedToken) {
+            // let uid = decodedToken.uid;
+            // console.log({uid})
+            // ...
+            const emailId = decodedToken.email;
+            const emailQuery = req.query.email;
+    
+            console.log(emailId, emailQuery);
+    
+            if(emailId === emailQuery){
+               orderCollection.find({email: emailQuery})
+              .toArray( (err, documents) => {
+                  res.status(200).send(documents);
+              })
+            }
+            else{
+              res.status(401).send('Un-authorized access request');
+            }
+          }).catch(function(error) {
+            // Handle error
+            res.status(401).send('Un-authorized access request');
+          });
+        }
+        else{
+          res.status(401).send('Un-authorized access request');
+        }
+          // booking.find({email: req.query.email})
+          // .toArray( (err, documents) => {
+          //     res.send(documents);
+          // })
+      })
 });
-
-
-app.get('/', (req, res) => {
-    res.send("Hello Server");
-})
 
 app.listen(process.env.PORT || port)
